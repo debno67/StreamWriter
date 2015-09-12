7d6ead8fe3659e0782f2636d4233663cece0ebb6
debno67 <debno67@cox.net>
2015-09-12 13:31:48 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace fileWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            //Create an instance of StreamWriter to write to a file
+            StreamWriter writer = new StreamWriter("..\\..\\Answers.txt");
+                    //the using statement will auto-close the file for us
+            using (writer)
+            {
+                //write a line
+                writer.WriteLine("Hello World");
+            }
+        }
+    }
+}
